cee65aba58299c9c66bb3c52573d76e787bb0d2f
Vivek Sah <viveks.xyz.com>
2020-01-25 14:28:54 -0800

    Initialize with gradle

---
+++ src/main/java/xyz/viveks/simpledb/DbException.java
@@ -0,0 +1,12 @@
+package xyz.viveks.simpledb;
+
+import java.lang.Exception;
+
+/** Generic database exception class */
+public class DbException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public DbException(String s) {
+        super(s);
+    }
+}
---
+++ src/test/java/xyz/viveks/simpledb/systemtest/SimpleDbTestBase.java
@@ -0,0 +1,20 @@
+package xyz.viveks.simpledb.systemtest;
+
+import org.junit.Before;
+
+import xyz.viveks.simpledb.Database;
+
+/**
+ * Base class for all SimpleDb test classes. 
+ * @author nizam
+ *
+ */
+public class SimpleDbTestBase {
+	/**
+	 * Reset the database before each test is run.
+	 */
+	@Before	public void setUp() throws Exception {					
+		Database.reset();
+	}
+	
+}
